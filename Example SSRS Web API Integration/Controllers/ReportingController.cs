af50e2087c1266e3b0b2b20c1ecdd1b218d94b8e
Chris Briggs <chrisbriggs@ssw.com.au>
2015-04-09 22:51:38 +1000

    Removed unneeded endpoint and added nuget package
    PM> Install-Package MicosoftReportViewerWebForms_v11

---
+++ Example SSRS Web API Integration/Controllers/ReportingController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Example_SSRS_Web_API_Integration.Controllers
+{
+    public class ReportingController : ApiController
+    {
+
+    }
+}
